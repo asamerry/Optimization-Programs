40db243016116b7bdd8f218bbda442df58bb876c
asamerry <asamerry72@gmail.com>
2025-02-12 17:08:12 -0800

    Added main.py and taylorSeries.py files; removed main functionality from gradientDescent.py

--- gradientDescent.py
+++ gradientDescent.py
@@ -1,46 +1,54 @@
 import numpy as np
-from sympy import symbols, diff, Matrix, sympify
+from sympy import diff, Matrix, sympify
 
-# Get function from input
-func_str = input("Enter a function f: R^n -> R ::: ")
-f = sympify(func_str)
 
-# Sets proper variables
-var_names = sorted(set(filter(str.isalpha, func_str)))
-vars = symbols(var_names)
+def gradientDescent(f, vars, epsilon:float = 10e-25, max_iters:int = 500, display:bool = True) -> None:
+ 
+    # Initial Guess
+    print("Initial Guess:")
+    x_0 = np.array([float(input(f"{var} = ")) for var in vars])
 
-# Defines the gradient of f(x)
-grad_f = Matrix([diff(f, x) for x in vars])
+    # Defines the gradient of f(x)
+    grad_f = Matrix([diff(f, x) for x in vars])
 
-# Search Parameters
-alpha = 0.05
-epsilon = 10e-25
-max_iters = 500
+    # Defines the Hessain of f(x)
+    H = Matrix([[diff(f, x) for x in vars] for f in grad_f])
 
-# Initial Guess
-print("Initial Guess:")
-x_0 = np.array([float(input(f"x{i+1} = ")) for i, _ in enumerate(vars)])
+    sequence = [x_0]
 
-sequence = [x_0]
+    for _ in range(max_iters):
+        grad_f_at_x_k = np.array([grad_f[i].subs(dict(zip(vars, sequence[-1]))) for i in range(len(grad_f))], dtype=np.float64)
 
-for _ in range(max_iters):
-    grad_f_at_x_k = np.array([grad_f[i].subs(dict(zip(vars, sequence[-1]))) for i in range(len(grad_f))], dtype=float)
+        H_x_k = H.subs(dict(zip(vars, sequence[-1])))
+
+        if H_x_k.det() != 0:
+            alpha = H_x_k.inv()
+
+            # Gradient Descent Algorithm: x_k+1 = x_k - alpha * grad_f(x_k)
+            new_x = sequence[-1] - alpha @ grad_f_at_x_k
+        
+        else:
+            print("Non-singular Hessian, using constant step size.")
+            
+            alpha = 0.05
+            
+            new_x = sequence[-1] - alpha * grad_f_at_x_k
+        
     
-    # Gradient Descent Algorithm: x_k+1 = x_k - alpha * grad_f(x_k)
-    new_x = sequence[-1] - alpha * grad_f_at_x_k
+        ### TODO: add "if ConditionsHold():" statement
 
-    ### TODO: add "if ConditionsHold():" statement
-
-    print(f"{len(sequence)}: {new_x}")
-    sequence.append(new_x)
+        if display:
+            print(f"Iteration {len(sequence)}: {new_x}")
+        
+        sequence.append(new_x)
     
-    # Termination condition: |f(x_k+1) - f(x_k)| < epsilon
-    if abs( f.subs(dict(zip(vars, sequence[-1]))) - f.subs(dict(zip(vars, sequence[-2])))) < epsilon:
-        break
-
-    for x in sequence[-1]:
-        if x == float("inf") or x == float("-inf"):
+        # Termination condition: |f(x_k+1) - f(x_k)| < epsilon
+        if abs(f.subs(dict(zip(vars, sequence[-1]))) - f.subs(dict(zip(vars, sequence[-2])))) < epsilon:
             break
 
-print(f"\nMinimizer: x* = {sequence[-1]}")
-print(f"Minimum: f(x*) = {f.subs(dict(zip(vars, sequence[-1])))}")
+        for x in sequence[-1]:
+            if x == float("inf") or x == float("-inf"):
+                break
+
+    print(f"\nMinimizer: x* = {sequence[-1]}")
+    print(f"Minimum: f(x*) = {f.subs(dict(zip(vars, sequence[-1])))}")
---
+++ taylorSeries.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sympy import symbols, diff, Matrix, sympify
+
+def taylorSeries(f, vars, x_0):
+    # Define the gradient and Hessian of the function
+    grad_f = np.array([diff(f, var) for var in vars])
+    try:
+        H = np.array([[diff(g, var) for var in vars] for g in grad_f])
+    except:
+        return False
+
+    # Evaluate each part at x_0
+    f_x_0 = f.subs(dict(zip(vars, x_0)))
+    grad_f_x_0 = np.array([grad_f_k.subs(dict(zip(vars, x_0))) for grad_f_k in grad_f])
+    H_x_0 = np.array([[H_ij.subs(dict(zip(vars, x_0))) for H_ij in H_i] for H_i in H])
+
+    # Define an arbitrary vector x
+    #xs = [f"x{i+1}" for i in range(len(vars))]
+    #x = np.array([x for x in symbols(xs)])
+    
+    taylorPolynomial = f_x_0 + (vars - x_0).T @ grad_f_x_0 + (1/2) * (vars - x_0).T @ H_x_0 @ (vars - x_0)
+
+    return taylorPolynomial
