fa5608ac56db1322aa58626567c0afa1fce014fd
asamerry <asamerry72@gmail.com>
2025-02-24 17:51:10 -0800

    Added leastSquares.py

---
+++ leastSquares.py
@@ -0,0 +1,17 @@
+# This program is used to solve equations of the form min ||Ax-b|| where x* = (A^TA)\invA^Tb
+
+import numpy as np
+
+
+def main():
+    A = np.array([[1, 2],
+                  [1, 1],
+                  [3, 2]])
+    b = np.array([6, 4, 5])
+
+    opt_x = np.linalg.inv(A.T @ A) @ A.T @ b
+
+    print(f"x* = {opt_x}")
+
+if __name__ == "__main__":
+    main()
