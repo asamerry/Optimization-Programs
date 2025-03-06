df8df2ad6f669b761de7c69961c140a8a447bdaa
asamerry <asamerry72@gmail.com>
2025-03-06 15:59:50 -0800

    Added more comments; updated mod function

--- kaczmarzsAlgorithm.py
+++ kaczmarzsAlgorithm.py
@@ -1,32 +1,29 @@
 # This program is used to find a solution to a system of linear equations with minimum norm
-# i.e. solving min ||x|| s.t. Ax=b where A is mxn and m \leq n with rank(A)=m
+# i.e. solving min ||x|| s.t. Ax=b where A is mxn and m \leq n with rank(A)=m.
+# Aswell, the parameter mu defines an over or undershoot of each projection between hyperplanes, 
+# with exact orthogonal projection when mu=1.
 
 import numpy as np
 
-
+# Similar to normal modulus function, but returns m if m|m
 def mod(k, m):
-    if k%m == 0:
-        return m
-    return k%m
+    return m if k%m == 0 else k%m
 
-def kaczmarzsAlgorithm():
+def kaczmarzsAlgorithm(iterations:int, mu:float = 1):
     # Initializes A and B
     A = np.array([[1, 0, 1, 0],
                   [0, 1, 1, 1]])
     b = np.array([2, 1])
-    
-    # Sets number of iterations and value of mu for over or undershooting the actual projection
-    iters = 5
-    mu = 1
 
+    # Define dimensions and initial guess of x0 = 0
     m, n = A.shape
     x = [np.zeros(n)]
     print(f"x0 = {x[0]}")
 
-    for k in range(iters):
+    # Iterates using Kaczmarz's algorithm
+    for k in range(iterations):
         x.append( x[k] + mu * (( b[mod(k+1, m)-1] - A[mod(k+1, m)-1].T @ x[k] ) / (A[mod(k+1, m)-1].T @ A[mod(k+1, m)-1])) * A[mod(k+1, m)-1] )
         print(f"x{k+1} = {x[k+1]}")
-    
 
 if __name__ == "__main__":
-    kaczmarzsAlgorithm()
+    kaczmarzsAlgorithm(iterations=5, mu=1)
